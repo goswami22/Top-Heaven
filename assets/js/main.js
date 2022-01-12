1dc7f8886e3a73d851c44874860501ee34cf730a
Bhavesh <goswamibhavesh22@gmail.com>
2022-01-12 10:20:50 +0530

    Top heaven - building web app

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+$('.property-list .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('.comment .owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        },
+        2000:{
+            item:3
+        }
+        
+    }
+})
+
